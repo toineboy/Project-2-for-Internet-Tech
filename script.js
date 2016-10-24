93d9133edfdcf8a598ff8d59d301e55bc98ffef0
toineboy <antoinebarriere2014@gmail.com>
2016-10-24 12:27:18 -0500

    Cat Lovers Questions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert("Hey Teddy! :)");
+
+var pictureShown = false;
+
+$(document).ready(function(){
+    $("#aristocat").click(function(){
+        pictureShown = !pictureShown;
+        $("#portrait").slideToggle();
+    });
+    
+    $("#portrait").click(function(){
+        if(pictureShown==true){
+            $("#output").html("The picture is shown.");
+        }else{
+            $("#output").html("No picture visible.");
+        }
+    });
+});
